bf52ce78924a9c54e88410b5c08fd2f94739c3ec
kmai12 <kevinmai993@yahoo.com>
2014-10-11 21:30:23 -0700

    Made /guestbook look better

--- assets/javascript/contacts.js
+++ assets/javascript/contacts.js
@@ -16,46 +16,19 @@ var bio = {
     "bioPic": "images/fry.jpg"
 }
 
-function displayBio(){
+function displayBottomContacts(){
     
-    var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
-    var formattedWelcome = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
-    var formattedName = HTMLheaderName.replace("%data%", bio.name);
-    var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
     var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
     var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
     var formattedGitHub = HTMLgithub.replace("%data2%", bio.contacts.github);
     formattedGitHub = formattedGitHub.replace("%data%", bio.contacts.githublink);
     var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
-
-
-    $("#header").prepend(formattedRole);
-    $("#header").prepend(formattedName);
-    $("#topContacts").append(formattedMobile);
-    $("#topContacts").append(formattedEmail);
-    $("#topContacts").append(formattedLocation);
-    $("#topContacts").append(formattedGitHub);
-
+    
     $("#footerContacts").append(formattedMobile);
     $("#footerContacts").append(formattedEmail);
     $("#footerContacts").append(formattedLocation);
     $("#footerContacts").append(formattedGitHub);
-
-    $("#header").append(formattedBioPic);
-    $("#header").append(formattedWelcome);
-
-
-
-    if(bio.skills.length > 0){
-	
-	$("#header").append(HTMLskillsStart);
-	for(skill in bio.skills){
-	    var formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
-	    $("#skills").append(formattedSkill);
-	}
-
-    }
+    
 }
 
-
-displayBio();
+displayBottomContacts();
